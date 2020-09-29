0eabd9b44ffa8371f7e64103b2ccc4a76c187584
Ananthan D <ananthan.d6@gmail.com>
2020-09-29 11:50:43 +0000

    Add project files.

---
+++ AzFunctionwithServicebus/ServiceBusQueueFunc.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+
+namespace AzFunctionwithServicebus
+{
+    public class ServiceBusQueueFunc
+    {
+
+        [FunctionName("ServicebusQueueFunc")]
+        public static void Run([ServiceBusTrigger("testq", Connection = "connectionString")] string myQueueItem, ILogger log)
+        {
+            log.LogInformation($"C# ServiceBus queue trigger function processed message: {myQueueItem}");
+            //Console.WriteLine(myQueueItem);
+        }
+    }
+}
